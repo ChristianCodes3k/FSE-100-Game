a20215888b00bdf1298383a1707bef4976f691ad
vadim <vadimnosenko9@gmail.com>
2024-11-21 14:06:37 -0700

    implimented breakout + other changes to backgrounds and keyboard herop

--- sketch.js
+++ sketch.js
@@ -12,6 +12,32 @@ let randY;
 let score = 0;
 let screen = 1;
 
+// variables for Breakout Game
+const windowWidth = 400;
+const windowHeight = 400;
+const rows = 6;
+const cols = 10;
+let rightDown = false;
+let leftDown = false;
+let alive = true;
+const brickWidth = Math.round(windowWidth/cols - 4);
+const brickHeight = Math.round((windowHeight * 1/3) / rows - 10);
+let bricks = [];
+let breakoutScore = 0;
+let paddle = {
+  x: windowWidth/2 - 50,
+  y: windowHeight - 15,
+  width: 100,
+  height: 10
+}
+let ball = {
+  x: paddle.x - 25,
+  y: paddle.y - 50,
+  speedX: 6,
+  speedY: 6,
+  diameter: 15
+}
+
 // variables for Keyboard Hero Game
 let notes = [];
 let points = 0;
@@ -29,6 +55,8 @@ let streakRoom = 2;
 const sound = new Audio("miss.mp3"); 
 const sounds = [new Audio("C5.mp3"), new Audio("E5.mp3"), new Audio("G5.mp3"), new Audio("A5.mp3")];
 let correct = new Audio();
+let noteAmount = 0;
+let highestStreak = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -58,12 +86,10 @@ function setup() {
 }
 
 function draw() {
-  background(0, 153, 255);
-
+  background(106,13,173);
   if (screen === 1){
-    background(106,13,173);
     fill(255,255,255);
-    
+    textAlign(LEFT);
     rect(40,60,110,60);
     rect(240,60,110,60);
     rect(40,280,110,60);
@@ -91,8 +117,16 @@ function draw() {
     if(mouseX >= 240 && mouseX <= 350 && mouseY >= 60 && mouseY <= 120 && mouseIsPressed){
       screen = 4;
     }
-    if(mouseX >= 240 && mouseX <= 350 && mouseY >= 280 && mouseY <= 340 && mouseIsPressed){
+
+    //Starts Breakout
+    if(mouseX >= 40 && mouseX <= 150 && mouseY >= 280 && mouseY <= 340 && mouseIsPressed){
+      generateBricks();
       screen = 7;
+    }
+
+    //Starts Keyboard Hero
+    if(mouseX >= 240 && mouseX <= 350 && mouseY >= 280 && mouseY <= 340 && mouseIsPressed){
+      screen = 8;
     }
 
   }
@@ -116,7 +150,6 @@ function draw() {
 
   if (screen === 3 || gameOver) {
     // Game complete screen
-    background(106,13,173);
     textSize(32);
     fill(0);
     //textAlign(CENTER, CENTER);  // Center the game complete text
@@ -146,7 +179,6 @@ function draw() {
   }
 
   if (screen === 4){
-    background(220);
     
     textSize(50);
     fill(255);
@@ -162,7 +194,6 @@ function draw() {
   }
 
   if (screen === 5){
-    background(220);
     textSize(50);
     text('Score: ', 100, 50);
     text(score, 250, 50);
@@ -187,7 +218,6 @@ function draw() {
   }
 
   if (screen === 6){
-    background(220);
     textSize(50);
     fill(255);
     rect(130,250,140,70);
@@ -203,6 +233,20 @@ function draw() {
     }
   }
   if (screen == 7){
+    if(bricks.length === 0){
+      endScreen("YOU WIN");
+    }
+    if(!alive && bricks.length != 0){
+      endScreen("GAME OVER");
+    }
+    if(alive){
+      drawBricks();
+      drawPaddle();
+      drawBall();
+      displayScore();
+     } 
+  }
+  if (screen == 8){
     if(!start) {
       fill(255, 255, 255);
       textSize(30);
@@ -210,14 +254,21 @@ function draw() {
       checkStart(); 
     }
     if(start) {
-      drawButtons();
-      notes.forEach(drawNote);
-      if(pause) {
+      if(notes.length == 0 && noteAmount == 100) {
         fill(255, 255, 255);
-        text("                Paused\nPress Back to go to menu", 80, 200);
+        textSize(30);
+        pause = true;
+        text("          Song End!\n\n\n\n           Points: " + points + "\n     Highest Streak: " + highestStreak + "\nPress Back to go to menu", 40, 100);
       } else {
-        notes.forEach(moveDown);
-        upkeep();
+        drawButtons();
+        notes.forEach(drawNote);
+        if(pause) {
+          fill(255, 255, 255);
+          text("                Paused\nPress Back to go to menu", 80, 200);
+        } else {
+          notes.forEach(moveDown);
+          upkeep();
+        }
       }
     }
   }
@@ -337,109 +388,143 @@ function drawButtons() {
   text("Points: " + points, 100, 370);
   text("Streak: " + streak, 220, 370);
   fill(255, 255, 255);
-  window.onkeydown = function(e){if(e.keyCode === 37){fill(255, 0, 0)}}
   triangle(77, 307.5, 113, 290, 113, 325);
-  fill(255, 255, 255);
-  window.onkeydown = function(e){if(e.keyCode === 38){fill(0, 255, 0, 50)}}
   triangle(147, 325, 165, 290, 183, 325);
-  fill(255, 255, 255);
-  window.onkeydown = function(e){if(e.keyCode === 40){fill(0, 0, 255, 50)}}
   triangle(217, 290, 235, 325, 253, 290);
-  fill(255, 255, 255);
-  window.onkeydown = function(e){if(e.keyCode === 39){fill(128, 0, 128, 50)}}
   triangle(287, 290, 287, 325, 324, 307.5); 
 }
 function upkeep() {
-  if(delay == delayGoal) {
-    newNote();
-    if(delayStreak == delayCurrent) {
-      delayGoal = Math.floor(Math.random()*3)*10+delayRoom;
-      delayStreak = Math.floor(Math.random()*3)+streakRoom;
-      delayCurrent = 0;
+  if(noteAmount != 100) {
+    if(delay == delayGoal) {
+      newNote();
+      if(delayStreak == delayCurrent) {
+        delayGoal = Math.floor(Math.random()*3)*10+delayRoom;
+        delayStreak = Math.floor(Math.random()*3)+streakRoom;
+        delayCurrent = 0;
+      }
+      delay = 0;
+      delayCurrent++;
     }
-    delay = 0;
-    delayCurrent++;
+    delay++;
   }
-  delay++;
 }
 
 function newNote() {
   a = Math.floor(Math.random()*4);
   b = new Note(a);
   notes.push(b);
+  noteAmount++;
 }
 
 function keyPressed() {
-  let found = false;
-  i = 0;
-  dir = -1;
-  switch(key) {
-    case 'ArrowLeft':
-      dir = 0;
-      break;
-    case 'ArrowUp':
-      dir = 1;
-      break;
-    case 'ArrowDown':
-      dir = 2;
-      break;
-    case 'ArrowRight':
-      dir = 3;
-      break; 
-    case 'Escape':
-      if(start) {
-        if(pause) {
-          pause = false;
+  if(screen == 7) {
+    if(keyCode === RIGHT_ARROW) {
+      rightDown = true;
+    }
+    if(keyCode === LEFT_ARROW) {
+      leftDown = true;
+    }
+    
+    if(keyCode === 32 && !alive){
+      alive = true;
+      paddle.x = windowWidth / 2 - 50,
+        ball.x = paddle.x - 25,
+        ball.y = paddle.y - 50,
+        ball.speedX = 5;
+        ball.speedY = 6;
+      bricks.splice(0, bricks.length);
+      breakoutScore = 0;
+      generateBricks();
+    }
+    if(key === 'Escape' && !alive) {
+      alive = true;
+      paddle.x = windowWidth / 2 - 50,
+        ball.x = paddle.x - 25,
+        ball.y = paddle.y - 50,
+        ball.speedX = 5;
+        ball.speedY = 6;
+      bricks.splice(0, bricks.length);
+      breakoutScore = 0;
+      screen = 1;
+    }
+  }
+  if(screen == 8) {
+    let found = false;
+    i = 0;
+    dir = -1;
+    switch(key) {
+      case 'ArrowLeft':
+        dir = 0;
+        break;
+      case 'ArrowUp':
+        dir = 1;
+        break;
+      case 'ArrowDown':
+        dir = 2;
+        break;
+      case 'ArrowRight':
+        dir = 3;
+        break; 
+      case 'Escape':
+        if(start) {
+          if(pause) {
+            pause = false;
+          } else {
+            pause = true;
+          }
         } else {
-          pause = true;
+          if(screen === 8) {
+            screen = 1;
+          }
         }
-      } else {
-        if(screen === 7) {
-          screen = 1;
+        return;
+      case 'Backspace':
+        if(pause) {
+          start = false;
+          pause = false;
+          notes = [];
+          points = 0;
+          streak = 0;
+          delay = 0;
+          delayGoal = 40;
+          delayStreak = 1;
+          delayCurrent = 1;
+          noteAmount = 0;
+          highestStreak = 0;
         }
-      }
-      return;
-    case 'Backspace':
-      if(pause) {
-        start = false;
-        pause = false;
-        notes = [];
-        points = 0;
-        streak = 0;
-        delay = 0;
-        delayGoal = 40;
-        delayStreak = 1;
-        delayCurrent = 1;
-      }
-      return
-    default:
-      return;
-  }
-  while(!found && i < notes.length) {
-    if(notes[i].getDir() == dir) {
-      found = true;
-      if(325-notes[i].getY() < 20) {
-        correct.pause();
-        correct.currentTime = 0
-        correct = sounds[dir];
-        correct.play();
-        points++;
-        streak++;
-        notes.splice(i, 1);
-      } else {
-        if(points != 0) {
-          points--;
-        }
-        streak = 0;
-      }
+        return
+      default:
+        return;
     }
-    i++;
-  }
-  if(!found) {
-    if(points != 0) {
-      points--;
+    while(!found && i < notes.length) {
+      if(notes[i].getDir() == dir) {
+        found = true;
+        if(325-notes[i].getY() < 20) {
+          correct.pause();
+          correct.currentTime = 0
+          correct = sounds[dir];
+          correct.play();
+          points++;
+          streak++;
+          if(streak > highestStreak) {
+            highestStreak = streak;
+          }
+          notes.splice(i, 1);
+        } else {
+          if(points != 0) {
+            points--;
+          }
+          streak = 0;
+        }
+      }
+      i++;
     }
-    streak = 0;
+    if(!found) {
+      if(points != 0) {
+        points--;
+      }
+      streak = 0;
+    }
   }
 }
 
@@ -494,3 +579,103 @@ class Note {
     this.y = y;
   }
 }
+
+//code for Breakout
+
+function keyReleased() {
+  if(screen == 7) {
+    if(keyCode === RIGHT_ARROW) {
+      rightDown = false;
+    }
+    if(keyCode === LEFT_ARROW) {
+      leftDown = false;
+    }
+  }
+}
+function generateBricks() {
+  for(let i = 0; i < rows; i++) {
+    for(let j = 0; j < cols; j++) {
+      let brickData = {
+        x: j * (brickWidth + 2) + 10,
+        y: i * (brickHeight + 2) + 30,
+        width: brickWidth,
+        height: brickHeight
+         }
+      bricks.push(brickData);
+      }
+   }
+}
+    
+     function drawBricks() {
+       bricks.forEach(brick=> {
+         fill('red');
+         rect(brick.x, brick.y, brick.width, brick.height);
+         noStroke();
+     })
+  }
+  function drawPaddle() {
+    fill('green');
+    rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    if(rightDown && paddle.x + paddle.width < windowWidth){
+      paddle.x += 10;
+    }
+    if(leftDown && paddle.x > 0) {
+      paddle.x -= 10;
+    }
+  }
+  function drawBall() {
+    fill('white');
+    circle(ball.x, ball.y, ball.diameter);
+    
+    if(ball.y - ball.diamter / 2 <= 40){
+      ball.speedY = -ball.speedY;
+    }
+    
+    if(ball.y + ball.diameter / 2 >= windowHeight) {
+      alive = false;
+    }
+     if(ball.x - ball.diameter / 2 <= 0||ball.x + ball.diameter / 2 >= windowWidth){
+       ball.speedX = -ball.speedX;
+     }
+    if(ball.y + ball.diameter / 2 >= paddle.y &&
+      ball.x >= paddle.x && ball.x < paddle.x + paddle.width/2){
+      ball.speedY = -ball.speedY;
+      if(ball.speedX > 0) {
+        ball.speedX = -ball.speedX;
+      }
+    }
+    if(ball.y + ball.diameter / 2 >= paddle.y && ball.x >= paddle.x + paddle.width/2 && ball.x < paddle.x + paddle.width){
+      ball.speedY = -ball.speedY;
+      if(ball.speedX < 0) {
+        ball.speedX = -ball.speedX;
+      }
+    }
+    bricks.forEach((brick, index) => {
+      if(ball.y - ball.diameter / 2 <= brick.y + brick.height && ball.x >= brick.x && ball.x <= brick.x + brick.width) {
+        ball.speedY = -ball.speedY;
+        bricks.splice(index, 1);
+        breakoutScore++;
+        if(bricks.length === 0) alive = false;
+      }
+    });
+      
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+  }
+  
+  function displayScore() {
+    fill("white"); 
+    textAlign(CENTER);
+    textSize(20);
+    text('Score: ' + breakoutScore, windowWidth / 2, 22);
+  }
+  
+  function endScreen(message) {
+    fill('white');
+    textAlign(CENTER);
+    textSize(24);
+    text(message, 200, 170);
+    text('press spacebar to restart game', 200, 225);
+    text('press escape to go to main menu', 200, 250);
+    text('score: ' + breakoutScore , 200, 200);
+  }
